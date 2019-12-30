d0e1e541755d7f0c168fd9e101cc77ee7f3ed4cb
avemike <fajny.michal.gajek@gmail.com>
2019-12-30 20:47:15 +0100

    waterball imgs placement

--- src/views/waterBall/WaterBall.js
+++ src/views/waterBall/WaterBall.js
@@ -9,8 +9,20 @@ export const WaterBall = () => {
         <div className="water water-back">
           <img src={process.env.PUBLIC_URL + "img/water_anim/water_back.png"}></img>
         </div>
+        <div className="clouds clouds-back">
+          <img src={process.env.PUBLIC_URL + "img/water_anim/clouds_back.png"}></img>
+        </div>
+        <div className="sun">
+          <img src={process.env.PUBLIC_URL + "img/water_anim/sun.png"}></img>
+        </div>
+        <div className="clouds clouds-front">
+          <img src={process.env.PUBLIC_URL + "img/water_anim/clouds_front.png"}></img>
+        </div>
         <div className="water water-front">
           <img src={process.env.PUBLIC_URL + "img/water_anim/water_front.png"}></img>
+        </div>
+        <div className="shine">
+          <img src={process.env.PUBLIC_URL + "img/water_anim/shine.png"}></img>
         </div>
       </div>
     </Panel>
