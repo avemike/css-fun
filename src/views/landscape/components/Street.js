c8fe9528c5fc9ab0ed128fb181d9d8773e272140
avemike <fajny.michal.gajek@gmail.com>
2020-01-06 21:56:38 +0100

    Street: road and road marking

--- src/views/landscape/components/Street.js
+++ src/views/landscape/components/Street.js
@@ -5,6 +5,22 @@ import '../styles/street.scss'
 export const Street = () => {
   return (
     <div className="street">
+      <div className="street-marking-group">
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+        <div className="street-line"></div>
+      </div>
       <Car />
     </div>
   )
