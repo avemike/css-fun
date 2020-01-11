1db9d1e88872de016ba7b2ff0601bce4184861b9
avemike <fajny.michal.gajek@gmail.com>
2020-01-11 22:26:23 +0100

    street landscape: mountains basic structure and location

--- src/views/landscape/components/Landscape/Landscape.js
+++ src/views/landscape/components/Landscape/Landscape.js
@@ -4,15 +4,25 @@ import './landscape.scss'
 
 import Panel from '../../../../components/showcasePanel';
 import { Street } from '../Street';
+import { Mountain } from '../Mountain';
 
 export const Landscape = () => {
+  const generateMountains = (n) => {
+    const Mountains = []
+    for(let i = 0; i < n; i++) {
+      Mountains.push(<Mountain order={i+1}/>)
+    }
+    return Mountains
+  }
+
   return (
     <Panel title="City Landscape">
       <div className="landscape">
         {/* First surface is background, last is foreground*/}
-        <div className="surface"></div>
-        <div className="surface"></div>
-        <div className="surface">
+        <div className="background">
+          { generateMountains(2) }
+        </div>
+        <div className="foreground">
           <Street/>
         </div>
       </div>
---
+++ src/views/landscape/components/Mountain/Mountain.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './mountain.scss'
+
+export const Mountain = props => {
+  return (
+    <div className={`mountain mountain-${props.order}`}>
+      
+    </div>
+  )
+}
