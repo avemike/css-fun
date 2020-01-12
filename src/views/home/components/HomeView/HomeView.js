d91d050057feba3e6b026fdd5080cbaeea22c226
avemike <fajny.michal.gajek@gmail.com>
2020-01-12 21:04:40 +0100

    minor changes and small improvements

--- src/views/home/components/HomeView/HomeView.js
+++ src/views/home/components/HomeView/HomeView.js
@@ -7,7 +7,7 @@ export const HomeView = () => {
     <Panel>
       <div className="home">
           <h1>Hello</h1>
-          <p>This project is kinda my personal CSS showcase and a place to play.</p>
+          <p>This project is kinda my personal CSS showcase and a place to play with my css-styling abilities.</p>
           <p>Frankly I wanted to create something cool. Something I can show other people for which I won't have to be ashamed.</p>
           <p>I'm using React, React-router and SCSS (not vanilla css, yes, but mainly I utilize from it nested selectors and variables)</p>
       </div>
--- src/views/waterball/components/Waterball/Waterball.js
+++ src/views/waterball/components/Waterball/Waterball.js
@@ -7,22 +7,22 @@ export const Waterball = () => {
     <Panel title="Water Ball Animation">
       <div className="water-ball">
         <div className="water water-back">
-          <img src={process.env.PUBLIC_URL + "img/water_anim/water_back.png"}></img>
+          <img src={process.env.PUBLIC_URL + "img/water_anim/water_back.png"} alt="Water waves in background of the animation"></img>
         </div>
         <div className="clouds clouds-back">
-          <img src={process.env.PUBLIC_URL + "img/water_anim/clouds_back.png"}></img>
+          <img src={process.env.PUBLIC_URL + "img/water_anim/clouds_back.png"} alt="Clouds in background of the animation"></img>
         </div>
         <div className="sun">
-          <img src={process.env.PUBLIC_URL + "img/water_anim/sun.png"}></img>
+          <img src={process.env.PUBLIC_URL + "img/water_anim/sun.png"} alt="Sun in centre of the animation"></img>
         </div>
         <div className="clouds clouds-front">
-          <img src={process.env.PUBLIC_URL + "img/water_anim/clouds_front.png"}></img>
+          <img src={process.env.PUBLIC_URL + "img/water_anim/clouds_front.png"} alt="Clouds in foreground of the animation"></img>
         </div>
         <div className="water water-front">
-          <img src={process.env.PUBLIC_URL + "img/water_anim/water_front.png"}></img>
+          <img src={process.env.PUBLIC_URL + "img/water_anim/water_front.png"} alt="Water waves in foreground of the animation"></img>
         </div>
         <div className="shine">
-          <img src={process.env.PUBLIC_URL + "img/water_anim/shine.png"}></img>
+          <img src={process.env.PUBLIC_URL + "img/water_anim/shine.png"} alt="Little shine on the right of waterball"></img>
         </div>
       </div>
     </Panel>
