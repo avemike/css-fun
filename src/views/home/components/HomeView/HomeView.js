defa7c67225bf99c54214482a7edd7d1cb723218
avemike <fajny.michal.gajek@gmail.com>
2020-01-12 20:05:58 +0100

    filled homeview with text about the project

--- src/views/home/components/HomeView/HomeView.js
+++ src/views/home/components/HomeView/HomeView.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Panel } from '../../../../components/showcasePanel/Panel'
+import './home.scss'
 
 export const HomeView = () => {
   return (
-    <div className="home">
-      <h1>
-        Hello
-      </h1>
-    </div>
+    <Panel>
+      <div className="home">
+          <h1>Hello</h1>
+          <p>This project is kinda my personal CSS showcase and a place to play.</p>
+          <p>Frankly I wanted to create something cool. Something I can show other people for which I won't have to be ashamed.</p>
+          <p>I'm using React, React-router and SCSS (not vanilla css, yes, but mainly I utilize from it nested selectors and variables)</p>
+      </div>
+    </Panel>
   )
 }
